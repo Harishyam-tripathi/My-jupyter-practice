61ce7046acfe9c86ca34a0e53c959a3500db1ef7
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2025-11-30 17:57:24 +0530

    Auto update: 30-11-2025 17:57:24.17

--- SQL_Basic/my_first_sql_script.sql
+++ SQL_Basic/my_first_sql_script.sql
@@ -1,12 +1,19 @@
-  CREATE DATABASE startersql;
-  USE startersql;
-  CREATE TABLE users (
-   id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    gender ENUM('Male', 'Female', 'Other'),
-    date_of_birth DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    SELECT * FROM users;
-    
+
+ SELECT email, gender FROM users;
+ --  Renaming a Table
+ rename table users to programmers;
+ select * from programmers;
+ rename table programmers to users;
+ select * from users;
+ -- ALTERING a Table
+  -- ADD a Column
+ alter table users add column is_active boolean default true;
+ select * from users;
+ -- DROP COLUMN
+ alter table users drop column is_active;
+ select * from users;
+ --  MODIFY a Column Type
+ ALTER TABLE users MODIFY COLUMN name VARCHAR(150);
+ SELECT * from users;
+  -- Move a Column to the First Position
+ 
