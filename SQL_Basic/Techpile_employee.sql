e01bbf672892131e233f999e903c117679d4a48e
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-02-01 02:47:50 +0530

    Auto update: 01-02-2026  2:47:49.99

---
+++ SQL_Basic/Techpile_employee.sql
@@ -0,0 +1,17 @@
+SELECT * FROM db_techpile.techpile_employee;
+insert into techpile_employee
+values ('Techpile technology', 9776857468, 'techpilelko@gmail.com' , 50000, '2026-09-30', 'male','Lucknow');
+
+insert into techpile_employee (name,email_id,salary)
+value ('Techpile','techpile@gmail.com',60000);
+
+insert into techpile_employee values('Sonam',9838841663, 'sonam@gmail.com',2000,'2026-01-30','male','lucknow');
+
+
+constraints -> primary key -> no duplicate entery 
+salary>0
+name -> not null 
+joining_date -> 2025-11-30
+gender -> focus on other values values data type 
+salary -> numeric -> not than digits
+insert into techpile_employee(name,email_id)value('Dikhsha','techpilelko@gmail.com')
